fff0cb6bc10dc60e5a15e34d0bad069718eb7cb4
EvertonBorges <evertonsoaresborges@gmail.com>
2020-05-15 02:45:39 -0400

    feat - Layout in Desktop and Mobile

---
+++ script.js
@@ -0,0 +1,12 @@
+const patterns = [
+    {
+        name: 'Tanya Sinclair',
+        profession: 'UX Engineer',
+        description: '" I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. "'
+    },
+    {
+        name: 'John Tarkpor',
+        profession: 'Junior Front-end Developer',
+        description: '" If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. "'
+    }
+]
