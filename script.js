7162c86c226e0f94d0440a837625b2a6b6c6e47e
EvertonBorges <evertonsoaresborges@gmail.com>
2020-05-15 03:13:41 -0400

    feat - Add script to switch between patterns

--- script.js
+++ script.js
@@ -9,4 +9,35 @@ const patterns = [
         profession: 'Junior Front-end Developer',
         description: '" If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. "'
     }
-]
+];
+
+let actualIndex = 0;
+
+function switchPattern(increment = 0) {
+    actualIndex += increment;
+    if (actualIndex > patterns.length) actualIndex = patterns.length - 1;
+    if (actualIndex < 0) actualIndex = 0;
+
+    const pattern = patterns[actualIndex];
+    const patternElement = document.getElementById('pattern');
+    const firstName = pattern.name.split(' ')[0].toLowerCase();
+
+    patternElement.innerHTML = `
+        <div class="pattern-texts">
+            <div class="pattern-text">
+            <label class="pattern-description">
+                ${pattern.description}
+            </label>
+            <label class="pattern-name">${pattern.name}</label>
+            <label class="pattern-profession">${pattern.profession}</label>
+            </div>
+        </div>
+        <div class="pattern-photo">
+            <img src="./images/image-${firstName}.jpg" alt="${pattern.description}">
+            <div class="navigation">
+            <a class="nav-left" onclick="switchPattern(-1)"><img src="./images/icon-prev.svg" alt="prev"></a>
+            <a class="nav-right" onclick="switchPattern(1)"><img src="./images/icon-next.svg" alt="next"></a>
+            </div>
+        </div>
+    `;
+}
